53dca888c4ed1e451670da5d4fef73f6834a703f
kris <nichol42@seattleu.edu>
2019-04-27 15:08:32 -0700

    all

--- empdata.js
+++ empdata.js
@@ -4,121 +4,121 @@
       "name": "George",
       "animal": "dog",
       "birthday": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "age": "2011/07/25",
+      "owner": "Tokyo",
+      "rescue_date": "8422"
     },
     {
       "name": "Brielle Williamson",
       "animal": "Integration Specialist",
       "birthday": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "age": "2012/12/02",
+      "owner": "New York",
+      "rescue_date": "4804"
     },
     {
       "name": "Ashton Cox",
       "animal": "Junior Technical Author",
       "birthday": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "age": "2009/01/12",
+      "owner": "San Francisco",
+      "rescue_date": "1562"
     },
     {
       "name": "Airi Satou",
       "animal": "Accountant",
       "birthday": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "age": "2008/11/28",
+      "owner ": "Tokyo",
+      "rescue_date": "5407"
     },
     {
       "name": "Caesar Vance",
       "animal": "Pre-Sales Support",
       "birthday": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "age": "2011/12/12",
+      "owner": "New York",
+      "rescue_date": "8330"
     },
     {
       "name": "Shad Decker",
       "animal": "Regional Director",
       "birthday": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "age": "2008/11/13",
+      "owner": "Edinburgh",
+      "rescue_date": "6373"
     },
     {
       "name": "Cedric Kelly",
       "animal": "Senior Javascript Developer",
       "birthday": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "age": "2012/03/29",
+      "owner": "Edinburgh",
+      "rescue_date": "6224"
     },
     {
       "name": "Haley Kennedy",
       "animal": "Senior Marketing Designer",
       "birthday": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "age": "2012/12/18",
+      "owner": "London",
+      "rescue_date": "3597"
     },
     {
       "name": "Colleen Hurst",
       "animal": "Javascript Developer",
       "birthday": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "age": "2009/09/15",
+      "owner": "San Francisco",
+      "rescue_date": "2360"
     },
     {
       "name": "Dai Rios",
       "animal": "Personnel Lead",
       "birthday": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "age": "2012/09/26",
+      "owner": "Edinburgh",
+      "rescue_date": "2290"
     },
     {
       "name": "Herrod Chandler",
       "animal": "Sales Assistant",
       "birthday": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "age": "2012/08/06",
+      "owner": "San Francisco",
+      "rescue_date": "9608"
     },
     {
       "name": "Rhona Davidson",
       "animal": "Integration Specialist",
       "birthday": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "age": "2010/10/14",
+      "owner": "Tokyo",
+      "rescue_date": "6200"
     },
     {
       "name": "Sonya Frost",
       "animal": "Software Engineer",
       "birthday": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "age": "2008/12/13",
+      "owner": "Edinburgh",
+      "rescue_date": "1667"
     },
     {
       "name": "Jena Gaines",
       "animal": "Office Manager",
       "birthday": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "age": "2008/12/19",
+      "owner": "London",
+      "rescue_date": "3814"
     },
     {
       "name": "Quinn Flynn",
       "animal": "Support Lead",
       "birthday": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "age": "2013/03/03",
+      "owner": "Edinburgh",
+      "rescue_date": "9497"
     }
   ]
 }
